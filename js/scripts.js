2e19263bb73ec6d5f2a15a25c89e8e2bb4aadba7
Brian Ongati <mrbrianomollo@gmail.com>
2021-09-21 01:57:57 +0300

    add crust prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,17 @@
 $(document).ready(function(){
 //insert js code here
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
+function Pizza(name) {
+    this.name = name;
+    this.price = 0;
+    this.quantity = 1;
+    this.toppings = [];
+}
+
+Pizza.prototype.setSize = function (size) {
+    const pizzaSize = pizzaSizes.find((pizzaSize) => pizzaSize.size == size);
+    if (pizzaSize) {
+        this.size = pizzaSize;
+        this.calculateTotal();
+    }
+};
